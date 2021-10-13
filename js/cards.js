e712fbf86fc5bd3bb2903b92f2d71dca43fa4ecd
Gormsdottir <uv_hist@mail.ru>
2021-10-13 16:37:42 +0300

    new file for the js data

---
+++ js/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: 'Толедо',
+      link: 'https://images.unsplash.com/photo-1468412526475-8cc70299f66f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      name: 'Касл Комб',
+      link: 'https://images.unsplash.com/photo-1580961074865-78eac6d26180?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=772&q=80'
+    },
+    {
+      name: 'Эдинбург',
+      link: 'https://images.unsplash.com/photo-1605641251481-e998506236a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      name: 'Квебек',
+      link: 'https://images.unsplash.com/photo-1524022523577-9005d7ae69d3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      name: 'Варшава',
+      link: 'https://images.unsplash.com/photo-1607427293702-036933bbf746?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1032&q=80'
+    },
+    {
+      name: 'Дубровник',
+      link: 'https://images.unsplash.com/photo-1559741145-8d23c9698ef2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    }
+];
